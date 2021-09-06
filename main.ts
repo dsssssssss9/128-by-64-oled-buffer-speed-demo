a594af2de4072642bcf7278df48f888503469a19
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2021-09-06 23:27:19 +0100

    tidy code for publication

--- main.ts
+++ main.ts
@@ -13,9 +13,9 @@ input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
     kitronik_VIEW128x64.clear()
     kitronik_VIEW128x64.refresh()
-    for (let index2 = 0; index2 <= 63; index2++) {
-        for (let index = 0; index <= 127; index++) {
-            kitronik_VIEW128x64.setPixelbuffer(index, index2)
+    for (let index22 = 0; index22 <= 63; index22++) {
+        for (let index3 = 0; index3 <= 127; index3++) {
+            kitronik_VIEW128x64.setPixelbuffer(index3, index22)
         }
     }
     kitronik_VIEW128x64.refresh()
@@ -23,6 +23,3 @@ input.onButtonPressed(Button.B, function () {
 })
 kitronik_VIEW128x64.clear()
 basic.showIcon(IconNames.Happy)
-basic.forever(function () {
-	
-})
